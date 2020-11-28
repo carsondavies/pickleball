fbc6fcb58549d01c9a72f340e8498bbcec38a6bc
Carson Davies <carsond11@gmail.com>
2020-11-27 20:02:32 -0700

    some minor render outline

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login: React.FC = () => {
 
+  const [state, setState] = useState({
+    username: '',
+    password: ''
+  })
+
   return (
-    <div>Login.tsx</div>
+    <div className='login-container'>Login.tsx
+      <input name='username' type='text' placeholder='username' />
+      <input name='password' type='passowrd' placeholder='password' />
+      <div className='button-container'>
+        <button>Login</button>
+        <button>Cancel</button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register: React.FC = () => {
-
+  const [state, setState] = useState({
+    username: '',
+    password: ''
+  })
 
   return (
-    <div>Register.tsx</div>
+    <div className='register-container'>
+      <input name='username' type='text' placeholder='username' />
+      <input name='password' type='passowrd' placeholder='password' />
+      <div className='button-container'>
+        <button>Register</button>
+        <button>Cancel</button>
+      </div>
+    </div>
   )
 }
 
